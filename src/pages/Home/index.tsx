dd6f480836912f4a9523b1b2591a0a234237e31b
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-21 17:44:24 -0300

    feat: add Home structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,38 @@
+import * as S from "./style";
+import { ReactComponent as Search } from "../../assets/icons/search.svg";
+
 const Home = () => {
-  return <div className="Home">Home</div>;
+  return (
+    <S.Home>
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Detalhes dos pedidos aqui</p>
+      </aside>
+    </S.Home>
+  );
 };
 
 export default Home;
