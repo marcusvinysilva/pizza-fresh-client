d2dbb62ea654b831ce768ce642a2a35f41634aaf
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-23 19:20:58 -0300

    feat(OrderDetails): insert OrderItem

--- src/components/OrderDetails/index.tsx
+++ src/components/OrderDetails/index.tsx
@@ -2,6 +2,7 @@ import * as S from "./style";
 import ButtonToggle from "components/ButtonToggle";
 import OrderItemList from "components/OrderItemList";
 import ButtonLarge from "components/ButtonLarge";
+import OrderItem from "components/OrderItem";
 
 const OrderDetails = () => {
   return (
@@ -21,7 +22,7 @@ const OrderDetails = () => {
               <h4>Preço</h4>
             </S.OrderDetailsListTitle>
           }
-          list={"Lista de Itens"}
+          list={<OrderItem />}
           footer={
             <S.OrderDetailsListFooter>
               <S.OrderDetailsListFooterRow>
