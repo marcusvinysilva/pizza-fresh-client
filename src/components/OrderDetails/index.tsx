b42027164d2b7a80293d3a8cdeb0e2332ef6e232
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-23 00:07:56 -0300

    feat(OrderDetails): insert OrderItemList

--- src/components/OrderDetails/index.tsx
+++ src/components/OrderDetails/index.tsx
@@ -1,5 +1,7 @@
 import * as S from "./style";
 import ButtonToggle from "components/ButtonToggle";
+import OrderItemList from "components/OrderItemList";
+import ButtonLarge from "components/ButtonLarge";
 
 const OrderDetails = () => {
   return (
@@ -11,7 +13,25 @@ const OrderDetails = () => {
         <ButtonToggle active={false} value="Delivery" />
       </S.OrderDetailsButtonGroup>
       <S.OrderDetailsList>
-        <p>Itens do pedido</p>
+        <OrderItemList
+          header={
+            <S.OrderDetailsListTitle>
+              <h4>Item</h4>
+              <h4>Qtd</h4>
+              <h4>Preço</h4>
+            </S.OrderDetailsListTitle>
+          }
+          list={"Lista de Itens"}
+          footer={
+            <S.OrderDetailsListFooter>
+              <S.OrderDetailsListFooterRow>
+                <span>Subtotal</span>
+                <span>R$ 10.00</span>
+              </S.OrderDetailsListFooterRow>
+              <ButtonLarge value="Continue para o pagamento" />
+            </S.OrderDetailsListFooter>
+          }
+        />
       </S.OrderDetailsList>
     </S.OrderDetails>
   );
