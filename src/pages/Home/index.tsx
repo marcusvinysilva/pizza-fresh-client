80a2fa9a801095debf2f16941cd9526ab369b2a2
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-22 23:56:20 -0300

    feat(home): insert OrderDetails

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,7 @@ import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
 import ProductItemList from "components/ProductItemList";
 import ProductItem from "components/ProductItem";
+import OrderDetails from "components/OrderDetails";
 
 const Home = () => {
   const dateDescription = DateTime.now().toLocaleString({
@@ -43,7 +44,7 @@ const Home = () => {
         </div>
       </S.HomeContent>
       <aside>
-        <p>Detalhes dos pedidos aqui</p>
+        <OrderDetails />
       </aside>
     </S.Home>
   );
