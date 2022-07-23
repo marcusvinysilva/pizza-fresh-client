8dd52ecb1a24dd02abbf6789602f8dcc6a643d49
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-22 23:59:50 -0300

    feat(OrderDetails): insert Button Toggle

--- src/components/OrderDetails/index.tsx
+++ src/components/OrderDetails/index.tsx
@@ -1,10 +1,15 @@
 import * as S from "./style";
+import ButtonToggle from "components/ButtonToggle";
 
 const OrderDetails = () => {
   return (
     <S.OrderDetails>
       <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
-      <S.OrderDetailsButtonGroup></S.OrderDetailsButtonGroup>
+      <S.OrderDetailsButtonGroup>
+        <ButtonToggle active={false} value="Comer no Local" />
+        <ButtonToggle active={false} value="P/ Viagem" />
+        <ButtonToggle active={false} value="Delivery" />
+      </S.OrderDetailsButtonGroup>
       <S.OrderDetailsList>
         <p>Itens do pedido</p>
       </S.OrderDetailsList>
