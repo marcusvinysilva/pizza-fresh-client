2995210823af9af00352d04af74dbc10ce9e4226
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-22 23:52:34 -0300

    feat: implement ProductItemList

--- src/components/ProductItemList/index.tsx
+++ src/components/ProductItemList/index.tsx
@@ -1,20 +1,24 @@
 import * as S from "./style";
 
-const ProductItemList = () => {
-	return (
-		<section>
-			<S.ProductItemListHeader>
-				<S.ProductItemListHeaderTitle>
-					Escolha os sabores
-				</S.ProductItemListHeaderTitle>
-				<S.ProductItemListHeaderSelect>
-					<option value="default">Selecione a mesa</option>
-					<option value="">Mesa</option>
-				</S.ProductItemListHeaderSelect>
-			</S.ProductItemListHeader>
-			<S.ProductItemList></S.ProductItemList>
-		</section>
-	);
+interface ProductItemListProps {
+  children: React.ReactNode;
+}
+
+const ProductItemList = ({ children }: ProductItemListProps) => {
+  return (
+    <section>
+      <S.ProductItemListHeader>
+        <S.ProductItemListHeaderTitle>
+          Escolha os sabores
+        </S.ProductItemListHeaderTitle>
+        <S.ProductItemListHeaderSelect>
+          <option value="default">Selecione a mesa</option>
+          <option value="">Mesa</option>
+        </S.ProductItemListHeaderSelect>
+      </S.ProductItemListHeader>
+      <S.ProductItemList>{children}</S.ProductItemList>
+    </section>
+  );
 };
 
 export default ProductItemList;
