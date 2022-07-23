1cc46e75ed4e40d3ec2891561b82dfbd984ca486
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-22 23:10:28 -0300

    feat(BoxLogin): add ButtonLarge

--- src/components/BoxLogin/index.tsx
+++ src/components/BoxLogin/index.tsx
@@ -1,22 +1,24 @@
 import logo from "assets/imgs/logo.png";
+import ButtonLarge from "components/ButtonLarge";
 import * as S from "./style";
 
 const BoxLogin = () => {
-	return (
-		<S.BoxLogin>
-			<S.BoxLoginLogo>
-				<S.BoxLoginLogoText>
-					<span>Pizza</span>
-					<span>Fresh</span>
-				</S.BoxLoginLogoText>
-				<S.BoxLoginLogoImage src={logo} alt="Logo" />
-			</S.BoxLoginLogo>
-			<S.BoxLoginForm>
-				<input type="text" placeholder="E-mail" />
-				<input type="password" placeholder="Senha" />
-			</S.BoxLoginForm>
-		</S.BoxLogin>
-	);
+  return (
+    <S.BoxLogin>
+      <S.BoxLoginLogo>
+        <S.BoxLoginLogoText>
+          <span>Pizza</span>
+          <span>Fresh</span>
+        </S.BoxLoginLogoText>
+        <S.BoxLoginLogoImage src={logo} alt="Logo" />
+      </S.BoxLoginLogo>
+      <S.BoxLoginForm>
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <ButtonLarge value="Entrar" type="button" />
+      </S.BoxLoginForm>
+    </S.BoxLogin>
+  );
 };
 
 export default BoxLogin;
