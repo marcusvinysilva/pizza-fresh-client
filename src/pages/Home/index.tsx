df81651c440649ce743bc90eef99e8f7d07fca53
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-22 22:42:29 -0300

    feat: add luxon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
 import Menu from "components/Menu";
 import { ReactComponent as Search } from "assets/icons/search.svg";
+import { DateTime } from "luxon";
 import * as S from "./style";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
 
 const Home = () => {
+  const dateDescription = DateTime.now().toLocaleString({
+    ...DateTime.DATE_SHORT,
+    weekday: "long",
+  });
+
   return (
     <S.Home>
       <Menu active={RoutePath.HOME} navItems={navigationItems} />
@@ -14,7 +20,7 @@ const Home = () => {
             <div>
               <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
               <S.HomeHeaderDetailsDate>
-                Aqui ficará a data
+                {dateDescription}
               </S.HomeHeaderDetailsDate>
             </div>
             <S.HomeHeaderDetailsSearch>
