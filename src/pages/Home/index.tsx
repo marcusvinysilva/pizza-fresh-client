10152548ec12947de270466ed22c71cfc8e3137a
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-24 12:48:25 -0300

    feat: configure navigation from Home to Settings

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import Menu from "components/Menu";
-import { ReactComponent as Search } from "assets/icons/search.svg";
 import { DateTime } from "luxon";
+import { ReactComponent as Search } from "assets/icons/search.svg";
 import * as S from "./style";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
@@ -9,16 +9,24 @@ import ProductItem from "components/ProductItem";
 import OrderDetails from "components/OrderDetails";
 import Overlay from "components/Overlay";
 import CheckoutSection from "components/CheckoutSection";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const dateDescription = DateTime.now().toLocaleString({
     ...DateTime.DATE_SHORT,
     weekday: "long",
   });
+  const navigate = useNavigate();
+  const handleNavigation = (path: RoutePath) => navigate(path);
 
   return (
     <S.Home>
-      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <Menu
+        active={RoutePath.HOME}
+        navItems={navigationItems}
+        onNavigate={handleNavigation}
+        onLogout={() => navigate(RoutePath.LOGIN)}
+      />
       <S.HomeContent>
         <header>
           <S.HomeHeaderDetails>
@@ -49,8 +57,8 @@ const Home = () => {
         <OrderDetails />
       </aside>
       {/* <Overlay>
-        <CheckoutSection />
-      </Overlay> */}
+                <CheckoutSection />
+            </Overlay> */}
     </S.Home>
   );
 };
