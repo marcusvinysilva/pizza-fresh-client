aa036a8981fbc7d3485c0667e3878a2e2ee60226
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-25 00:13:56 -0300

    feat: add selected table

--- src/components/ProductItemList/index.tsx
+++ src/components/ProductItemList/index.tsx
@@ -1,19 +1,24 @@
 import * as S from "./style";
 
 interface ProductItemListProps {
+  onSelectTable: (data: number) => void;
   children: React.ReactNode;
 }
 
-const ProductItemList = ({ children }: ProductItemListProps) => {
+const ProductItemList = ({ children, onSelectTable }: ProductItemListProps) => {
   return (
     <section>
       <S.ProductItemListHeader>
         <S.ProductItemListHeaderTitle>
           Escolha os sabores
         </S.ProductItemListHeaderTitle>
-        <S.ProductItemListHeaderSelect>
+        <S.ProductItemListHeaderSelect
+          onChange={({ target }) => onSelectTable(Number(target.value))}
+          name="table"
+          id="table"
+        >
           <option value="default">Selecione a mesa</option>
-          <option value="">Mesa</option>
+          <option value="01">Mesa 01</option>
         </S.ProductItemListHeaderSelect>
       </S.ProductItemListHeader>
       <S.ProductItemList>{children}</S.ProductItemList>
